736bd59cd55c7f3c37eea7ff688996da5d8d420d
SheriKarasick <61953736+SheriKarasick@users.noreply.github.com>
2020-06-04 17:10:44 -0400

    Add files via upload

--- plots.js
+++ plots.js
@@ -13,22 +13,12 @@ function init() {
 }
   
   init();
-
   function optionChanged(newSample) {
     buildMetadata(newSample);
     buildCharts(newSample);
     buildCharts2(newSample);
-    buildCharts3(newSample);
   }
 
-  function pageLoad(item) {
-    d3.json("samples.json").then((data) => {
-    buildMetadata(940);
-    buildCharts(940);
-    buildCharts2(940);  
-  });
-}
-window.onload = pageLoad();
 
   function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
@@ -108,6 +98,13 @@ window.onload = pageLoad();
             console.log(bacteria_id2);
             console.log(bacteria_names2);
                 
+            //build a bar chart
+            //var bubble_y_data = num_bacteria2;
+            //var bubble_x_data = bacteria_id2;
+            //console.log(bubble_x_data)
+            //console.log(bubble_y_data)
+
+
             var trace1 = {
                 x: bacteria_id2,
                 y: num_bacteria2,
@@ -116,7 +113,7 @@ window.onload = pageLoad();
                     marker: {
                         color: bacteria_id2,
                         size: num_bacteria2,
-                        colorScale: "Blackbody",
+                        colorScale: "Electric",
                         type: "heatmap"
                     }};
 
@@ -133,92 +130,3 @@ window.onload = pageLoad();
         });
     };
 
-//weak attempt at a gauge
-// Enter a speed between 0 and 180
-
-function buildCharts3(sample) {
-    d3.json("samples.json").then((data) => {
-            console.log("hello");
-        var metadata = data.metadata;
-        var samples = data.samples;
-        var gaugeValue = metadata[0].wfreq;
-        var resultsArray = samples.filter(sample_object => sample_object.id == sample);
-        var result = resultsArray[0];
-        console.log(metadata);
-        console.log(gaugeValue);
-
-    // Enter a speed between 0 and 180
-        var g = 180 / gaugeValue
-        console.log(g)
-
-    var level = g;
-
-    // Trig to calc meter point
-    var degrees = 180 - level,
-      radius = .5;
-    var radians = degrees * Math.PI / 180;
-    var aX = 0.025 * Math.cos((degrees - 90) * Math.PI / 180);
-    var aY = 0.025 * Math.sin((degrees - 90) * Math.PI / 180);
-    var bX = -0.025 * Math.cos((degrees - 90) * Math.PI / 180);
-    var bY = -0.025 * Math.sin((degrees - 90) * Math.PI / 180);
-    var cX = radius * Math.cos(radians);
-    var cY = radius * Math.sin(radians);
-
-    var mainPath = path1,
-     pathX = String(x),
-     space = ' ',
-     pathY = String(y),
-     pathEnd = ' Z';
-var path = mainPath.concat(pathX,space,pathY,pathEnd);
-
-    var data = [
-      {
-        values: [1,1,1,1,4],
-        rotation: 90,
-        text: ['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9'],
-        textinfo: 'text',
-        textposition: 'inside',
-        marker: {
-          colors: ['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
-            'rgba(249, 168, 37, .5)', 'rgba(183,28,28, .5)',
-            'rgba(0, 0, 0, 0.5)'
-          ]
-        },
-        hoverinfo: 'label',
-        hole: .5,
-        type: 'pie',
-        showlegend: false
-      }
-    ];
-
-    var layout = {
-      shapes: [{
-        type: 'path',
-        path: path,
-        fillcolor: '850000',
-        line: {
-          color: '850000'
-        }
-      }],
-      height: 400,
-      width: 400,
-      xaxis: {
-        zeroline: false,
-        showticklabels: false,
-        showgrid: false,
-				fixedrange: true,
-        range: [-1, 1]
-      },
-      yaxis: {
-        zeroline: false,
-        showticklabels: false,
-        showgrid: false,
-				fixedrange: true,
-        range: [-1, 1]
-      }
-    };
-
-    Plotly.newPlot('gauge', data, layout);
-//end gauge code
-  });
-}
